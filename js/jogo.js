9d352fd9bc006eb2f5f1c08b8fab94a8d1ddc065
Gabriel00021 <123033653+Gabriel00021@users.noreply.github.com>
2023-03-25 11:03:52 -0300

    Add files via upload

--- js/jogo.js
+++ js/jogo.js
@@ -1,5 +1,6 @@
 const cuphead = document.querySelector(".cuphead");
 const obstaculo = document.querySelector(".obstaculo");
+const gameOver = document.querySelector(".gameOver")
 
 const pulo = ()=>{
     cuphead.classList.add("pular")
@@ -7,5 +8,25 @@ const pulo = ()=>{
         cuphead.classList.remove("pular")
     },800)
 }
+
+const loop = setInterval(() => {
+    const obstaculoPosition = obstaculo.offsetLeft;
+    const cupheadPosition = +window.getComputedStyle(cuphead).bottom.replace('px', '')
+
+    console.log(cupheadPosition)
+    if(obstaculoPosition <= 100 && obstaculoPosition > 0 && cupheadPosition <= 100){
+
+        obstaculo.style.animation = 'none'
+        obstaculo.style.left = `${obstaculoPosition}px`;
+    
+        cuphead.style.anymation = 'none'
+        cuphead.style.left = `${cupheadPosition}px`
+
+       // gameOver.classList.remove(".apagar");
+
+        clearInterval(loop);
+    }
+}, 10);
+
 
 document.addEventListener("keydown",pulo);
